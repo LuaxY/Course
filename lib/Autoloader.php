81abc5febca926088f789c9f29911dae15ccb223
LuaxY <yann.guineau@voidmx.net>
2015-03-02 22:08:24 +0100

    Upgrade course (Part 3) Build navigation menu with Daux.io and Treeview

--- lib/Autoloader.php
+++ lib/Autoloader.php
@@ -2,9 +2,11 @@
 
 require_once "Parsedown/Parsedown.php";
 require_once "Twig/Autoloader.php";
+require_once "Daux.io/daux_helper.php";
+require_once "Daux.io/daux_directory.php";
 
 /**
- * URL
+ * Parse URL to display the requested page
  */
 $url  = explode('/', htmlentities(substr($_SERVER['PATH_INFO'], 1)));
 
@@ -24,7 +26,7 @@ function url($url = null)
 }
 
 /**
- * Parsedown
+ * Call Parsedown lib to convert Markdown to HTML
  */
 function markdown($text)
 {
@@ -33,10 +35,52 @@ function markdown($text)
 }
 
 /**
- * Twig
+ * List 'doc' folder to generate menus and submenus
+ **/
+function generate_menu()
+{
+    $tree = DauxHelper::build_directory_tree(ROOT."/doc");
+
+    $nav = '<ul class="tree">';
+    $nav .= build_navigation($tree, '', null, null);
+    $nav .= '</ul>';
+
+    echo $nav;
+}
+
+function build_navigation($tree, $path, $current_url, $base_page)
+{
+    $nav = '';
+    foreach ($tree->value as $node)
+    {
+    	$url = $node->uri;
+        if ($node->type === Directory_Entry::FILE_TYPE) {
+            if ($node->value === 'index') continue;
+            $nav .= '<li>';
+            $link = ($path === '') ? $url : $path . '/' . $url;
+            $nav .= '<a class="mod" href="' . $base_page . $link . '">' . $node->title . '</a></li>';
+        } else {
+            $nav .= '<li>';
+            $link = ($path === '') ? $url : $path . '/' . $url;
+            if ($node->index_page) $nav .= '<a class="mod" href="' . $base_page . $link . '">' . $node->title . '</a>';
+            else $nav .= '<a href="#" class="mod folder">' . $node->title . '</a></li>';
+            $nav .= '<ul>';
+            $new_path = ($path === '') ? $url : $path . '/' . $url;
+            $nav .= build_navigation($node, $new_path, $current_url, $base_page);
+            $nav .= '</ul></li>';
+        }
+    }
+
+    return $nav;
+}
+
+/**
+ * Call Twig lib to process template display
  */
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem(ROOT);
 $twig = new Twig_Environment($loader);
 $twig->addFunction(new Twig_SimpleFunction('url', 'url'));
 $twig->addFunction(new Twig_SimpleFunction('markdown', 'markdown'));
+$twig->addFunction(new Twig_SimpleFunction('dump', 'var_dump'));
+$twig->addFunction(new Twig_SimpleFunction('generate_menu', 'generate_menu'));
